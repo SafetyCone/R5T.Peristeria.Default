f78e1993a0b7046f8fd345c649e71a993d15efcb
MinexAutomation <minex.automation@gmail.com>
2020-01-30 13:47:21 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Peristeria.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Peristeria.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="DropboxDirectoryNameConvention"/> implementation of <see cref="IDropboxDirectoryNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultDropboxDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<IDropboxDirectoryNameConvention, DropboxDirectoryNameConvention>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DropboxDirectoryNameConvention"/> implementation of <see cref="IDropboxDirectoryNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IDropboxDirectoryNameConvention> AddDefaultDropboxDirectoryNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IDropboxDirectoryNameConvention>(() => services.AddDefaultDropboxDirectoryNameConvention());
+            return serviceAction;
+        }
+    }
+}
